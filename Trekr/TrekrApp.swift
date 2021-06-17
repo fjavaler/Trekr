449c7aaf8cb0fab2b980eb2d4039335c65153329
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-17 12:52:02 -0600

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Frederick Javalera on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
